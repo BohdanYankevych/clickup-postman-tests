02f4f333183a80a1f7d662b2dc73da7b424b9be3
Bohdan_Yankevych <777janek777@gmail.com>
2025-05-29 20:26:08 +0200

    delete

--- cypress-clickup-api/cypress.config.ts
+++ cypress-clickup-api/cypress.config.ts
@@ -4,12 +4,12 @@ import * as dotenv from 'dotenv';
 dotenv.config(); // Завантаження змінних з .env файлу
 
 export default defineConfig({
-  e2e: {
-    baseUrl: 'https://api.clickup.com/api/v2',
-    supportFile: 'cypress/support/commands.ts',
-  },
-  env: {
-    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-    TEAM_ID: process.env.TEAM_ID,
-  },
+    e2e: {
+        baseUrl: 'https://api.clickup.com/api/v2',
+        supportFile: 'cypress/support/commands.ts'
+    },
+    env: {
+        ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+        TEAM_ID: process.env.TEAM_ID
+    }
 });
